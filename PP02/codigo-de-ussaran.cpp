b6b50e16ad766f94caa6847aa974d042847c4ecc
Graziela Ralph <ralphgraziela25@gmail.com>
2026-05-25 14:34:59 -0400

    create Node, HashNode and main

--- PP02/codigo-de-ussaran.cpp
+++ PP02/codigo-de-ussaran.cpp
@@ -1,2 +1,44 @@
 #include <iostream>
 using namespace std;
+
+template<typename T>
+class Node {
+private:
+    T item;
+public:
+    Node<T>* next;
+    Node<T>* prev;
+    Node(): next(nullptr),prev(nullptr) {}
+    Node(T item): item(item), next(nullptr), prev(nullptr) {}
+    T& get_item();
+};
+
+template<typename T>
+T& Node<T>::get_item() { return item; }
+
+//----------------------------------------------------------------------------------------------------------------------------
+template<typename V, typename K>
+class HashNode{
+private:
+	K key;
+	V value;
+public:
+	
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------------
+<typename T>
+class HashTable{
+private:
+	T
+
+public:
+
+
+}
+
+int main(){
+
+	return 0;
+}
